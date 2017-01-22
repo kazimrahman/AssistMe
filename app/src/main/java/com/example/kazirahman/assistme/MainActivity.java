2d49f98bdf673b3497e10658b08c012bbeedb9b8
Haroon Syed <harsyed@gmail.com>
2017-01-22 05:07:15 -0800

    respectable build

--- app/src/main/java/com/example/kazirahman/assistme/MainActivity.java
+++ app/src/main/java/com/example/kazirahman/assistme/MainActivity.java
@@ -2,26 +2,15 @@ package com.example.kazirahman.assistme;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.Bundle;
 import android.view.View;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -49,10 +38,9 @@ public class MainActivity extends AppCompatActivity {
         login = (Button)findViewById(R.id.login_btn);
         logintxt = (EditText)findViewById(R.id.email_txt);
         password = (EditText)findViewById(R.id.password_txt);
-        loginButton = (LoginButton) findViewById(R.id.fb_login_button);
         textView = (TextView) findViewById(R.id.text_view);
         callbackManager = CallbackManager.Factory.create();
-        loginButton.setReadPermissions("public_profile","email","user_friends");
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,26 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                textView.setText("Login success \n" +
-                        loginResult.getAccessToken().getUserId() +
-                        "\n" + loginResult.getAccessToken().getToken());
-
-            }
-
-            @Override
-            public void onCancel() {
-                textView.setText("Login Cancelled");
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-
-            }
-        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
